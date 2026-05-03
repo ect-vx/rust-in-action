8adc124c2cee5a5f09014568a9e15979e09d9a21
rencorr <rencorr@mail.ru>
2026-05-03 14:13:22 +1000

    half 14 and 13

--- ch02/14-read-file.rs
+++ ch02/14-read-file.rs
@@ -3,20 +3,22 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
-  let f = File::open("readme.md").unwrap();    // <1>
+  let f = File::open("readme.md").unwrap();     // создание объекта File, получение результата 
+                                                // из структуры "Result" для безопасности
   let mut reader = BufReader::new(f);
 
-  let mut line = String::new();    // <2>
+  let mut line = String::new();    // создание изменяемого объекта для хранения строк
 
   loop {
     let len = reader.read_line(&mut line)
-                    .unwrap(); // <3>
+                    .unwrap();  // явная обработка "опасной" операции
+                                // - чтение с диска может быть проблемой
     if len == 0 {
       break
     }
 
     println!("{} ({} bytes long)", line, len);
 
-    line.truncate(0);    // <4>
+    line.truncate(0);    // 
   }
 }
